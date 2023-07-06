72e106477daf6813fec1b259bd8fbad9d6d4cbb2
Yeah'Lim <80014833+YeahLim@users.noreply.github.com>
2023-07-06 14:33:19 +0900

    [level 2] Title: 피로도, Time: 0.05 ms, Memory: 71.2 MB -BaekjoonHub

--- 프로그래머스/lv2/87946. 피로도/피로도.java
+++ 프로그래머스/lv2/87946. 피로도/피로도.java
@@ -10,33 +10,22 @@ class Solution {
             dungeons = DUNGEONS.clone();
         }
         visited = new boolean[dungeons.length];
-        searchDungeons(0, k, 0);
+        searchDungeons(k, 0);
         
         return answer;
     }
     
     /* DFS : 몇 개 던전 갈 수 있는지 탐색 */
-    private void searchDungeons(int depth, int fatigue, int count) {        
+    private void searchDungeons(int fatigue, int count) {        
         // 모든 던전을 다 탐색했을 때
-        if (depth == dungeons.length) {
-            answer = answer < count ? count : answer;
-            return;
-        }
-        
-        // 모든 던전을 갈 수 있을 때
-        if (answer == dungeons.length) {
-            return;
-        }
-        
+
+        answer = Math.max(answer, count);
+
         for (int i = 0; i < dungeons.length; i++) { 
             if (visited[i]) continue;
-            visited[i] = true;
-            if (fatigue >= dungeons[i][0]) {         
-                searchDungeons(depth + 1, fatigue - dungeons[i][1], count + 1);
-            }
-            else {
-                searchDungeons(depth + 1, fatigue, count);
-            }
+            if (fatigue < dungeons[i][0]) continue;
+            visited[i] = true;       
+            searchDungeons(fatigue - dungeons[i][1], count + 1);
             visited[i] = false;
         }
     }
