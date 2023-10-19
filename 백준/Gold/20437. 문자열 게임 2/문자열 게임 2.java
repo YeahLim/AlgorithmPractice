066a6566de5b939eb453ec9cb58b1b87321bf653
Yeah'Lim <80014833+YeahLim@users.noreply.github.com>
2023-10-20 00:15:17 +0900

    [Gold V] Title: 문자열 게임 2, Time: 484 ms, Memory: 20768 KB -BaekjoonHub

---
+++ 백준/Gold/20437. 문자열 게임 2/문자열 게임 2.java
@@ -0,0 +1,78 @@
+import java.io.*;
+import java.util.*;
+
+// 10:50
+class Main {
+	
+	static int num;
+	static String str;
+	static int min, max;
+	
+	public static void main(String[] args) throws Exception {
+		/* 입력 */
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int tc = Integer.parseInt(br.readLine());
+		
+		while(tc-- > 0) {
+			str = br.readLine();
+			num = Integer.parseInt(br.readLine());
+			
+			// 길이보다 숫자가 작은 경우
+			if  (num == 1) {
+				System.out.println("1 1");
+				continue;
+			}
+			
+			getStringLength();			
+			
+			/* 출력 */
+			if (min == Integer.MAX_VALUE) {
+				System.out.println(-1);
+			}
+			else {
+				System.out.println(min + " " + max);
+			}
+		}
+	}
+	
+	/* 가장 짧은/긴 문자열 길이 구하기 */
+	private static void getStringLength() {
+		int[] count = getAlphabetCount();
+		min = Integer.MAX_VALUE;
+		max = Integer.MIN_VALUE;
+		for (int i = 0; i < str.length(); i++) {
+			int left = str.charAt(i) - 'a';
+			
+			// 현재 문자의 개수가 num보다 클 때
+			if (count[left] >= num) {
+				int curCount = 1; // 현재 문자의 개수
+				for (int j = i+1; j < str.length(); j++) {
+					int right = str.charAt(j) - 'a';
+					
+					// 첫번째 문자와 마지막 문자가 같을때
+					if (left == right) {
+						curCount++;
+						
+						if (curCount == num) {
+							min = Math.min(min,  j - i + 1);
+							max = Math.max(max,  j - i + 1);
+							break;
+						}
+						
+					}
+				}
+			}			
+		}
+		
+	}
+
+	private static int[] getAlphabetCount() {
+		int[] count = new int[26];
+		
+		for (char s : str.toCharArray()) {
+//			System.out.println(s-'a');
+			count[s - 'a']++;
+		}
+		return count;
+	}
+}
