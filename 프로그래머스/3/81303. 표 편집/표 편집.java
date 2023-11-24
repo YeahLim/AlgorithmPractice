b3299d8018529e1042c82993c78932a13b6ffb64
Yeah'Lim <80014833+YeahLim@users.noreply.github.com>
2023-11-24 20:44:48 +0900

    [level 3] Title: 표 편집, Time: 328.29 ms, Memory: 141 MB -BaekjoonHub

---
+++ 프로그래머스/3/81303. 표 편집/표 편집.java
@@ -0,0 +1,91 @@
+import java.util.*;
+
+// 1. 선택 로직
+// 2. 삭제 로직
+
+class Solution {
+    public String solution(int n, int k, String[] cmd) {
+        
+        int pointer = k;
+        
+        Node[] nodes = new Node[1000001];
+        nodes[0] = new Node();
+        for (int i = 1; i < n; i++) {
+            nodes[i] = new Node();
+            nodes[i].prev = nodes[i-1];
+            nodes[i-1].next = nodes[i];
+        }
+        
+        Stack<Node> deleteStack = new Stack<>();
+        Node now = nodes[k];
+        for (String c : cmd) {
+            if (c.startsWith("U ")) {
+                int count = Integer.parseInt(c.split(" ")[1]); 
+                while (count-- > 0) {
+                    now = now.prev;
+                }
+            }
+            
+            else if (c.startsWith("D ")) {
+                int count = Integer.parseInt(c.split(" ")[1]); 
+                while (count-- > 0) {
+                    now = now.next;
+                }
+            }
+            
+            else if (c.equals("C")) {
+                now.deleted = true;
+                deleteStack.add(now);
+                
+                Node prev = now.prev;
+                Node next = now.next;
+                
+                if (prev != null) {
+                    prev.next = next;
+                }
+                if (next != null) {
+                    next.prev = prev;
+                    now = next;
+                }
+                else {
+                    now = prev;
+                }
+                
+            }
+            
+            else if (c.equals("Z")) {
+                Node node = deleteStack.pop();
+                node.deleted = false;
+                Node prev = node.prev;
+                Node next = node.next;
+                
+                if (prev != null) {
+                    prev.next = node;
+                }
+                
+                if (next != null) {
+                    next.prev = node;
+                }
+
+            }
+                
+        }
+        
+        StringBuilder answer = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            if (nodes[i].deleted) {
+                answer.append('X');
+            }
+            else {
+                answer.append('O');
+            }
+        }
+        
+        return answer.toString();
+    }
+}
+
+class Node{
+    Node prev, next;
+    boolean deleted;
+}
