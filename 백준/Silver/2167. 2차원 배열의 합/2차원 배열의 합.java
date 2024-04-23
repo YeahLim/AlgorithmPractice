994eedb130f5b3a4c8a60f124b90e0b55e7f29bc
Yeah'Lim <80014833+YeahLim@users.noreply.github.com>
2024-04-23 22:26:27 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 576 ms, Memory: 30108 KB -BaekjoonHub

--- 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.java
+++ 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.java
@@ -1,16 +1,10 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
 	
-	
-	static int[][] array;
 	
 	public static void main(String[] args) throws IOException {
 		
@@ -20,7 +14,7 @@ public class Main {
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 		
-		array = new int[N][M];
+		int[][] array = new int[N][M];
 	
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
