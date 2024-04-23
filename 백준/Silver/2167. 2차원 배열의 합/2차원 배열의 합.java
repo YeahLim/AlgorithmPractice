e3b9801e7a28c5349b8b831032f762a635fae2ec
Yeah'Lim <80014833+YeahLim@users.noreply.github.com>
2024-04-23 22:23:39 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 576 ms, Memory: 30400 KB -BaekjoonHub

---
+++ 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.java
@@ -0,0 +1,84 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	
+	static int[][] array;
+	
+	public static void main(String[] args) throws IOException {
+		
+		// 변수 초기화
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		array = new int[N][M];
+	
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < M; j++) {
+				array[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		
+		// 누적합 구하기
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				
+				if (i == 0 && j == 0) {
+					continue;
+				}
+				
+				if (i == 0) {
+					array[i][j] += array[i][j-1];
+				}
+				
+				else if (j == 0) {
+					array[i][j] += array[i-1][j];
+				}
+				
+				else {
+					array[i][j] += array[i-1][j] + array[i][j-1] - array[i-1][j-1];
+				}
+				
+			}
+		}
+		
+		
+		// 배열의 합 구하기
+		int K = Integer.parseInt(br.readLine());
+		while (K-- > 0) {
+			st = new StringTokenizer(br.readLine());
+			int i = Integer.parseInt(st.nextToken()) - 1;
+			int j = Integer.parseInt(st.nextToken()) - 1;
+			int x = Integer.parseInt(st.nextToken()) - 1;
+			int y = Integer.parseInt(st.nextToken()) - 1;
+			
+			int total = array[x][y];
+			
+			if (i >= 1) {
+				total -= array[i-1][y];
+			}
+			
+			if (j >= 1) {
+				total -= array[x][j-1];
+			}
+			
+			if (i >= 1 && j >= 1) {
+				total += array[i-1][j-1];
+			}
+			
+			System.out.println(total);
+		}
+		
+	}
+}
