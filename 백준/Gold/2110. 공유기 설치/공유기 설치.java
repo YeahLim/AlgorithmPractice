066dd534cef6428992026b7801d82bc9826e4957
Yeah'Lim <80014833+YeahLim@users.noreply.github.com>
2024-07-30 22:52:40 +0900

    [Gold IV] Title: 공유기 설치, Time: 912 ms, Memory: 99592 KB -BaekjoonHub

---
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -0,0 +1,58 @@
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	
+	static int[] house;
+	static int answer = 1;
+	
+	public static void main(String[] args) throws IOException {
+		
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int C = sc.nextInt();
+		
+		house = new int[N];
+		for (int i = 0; i < N; i++) {
+			house[i] = Integer.parseInt(sc.next());
+		}
+		Arrays.sort(house);
+		
+		int start = 1;
+		int end = house[N-1] - house[0];
+		
+		while (start <= end) {
+			int mid = (start + end) / 2;
+			
+			if (checkInstall(mid) >= C){
+				start = mid + 1;
+				answer = mid;
+			}
+			else {
+				end = mid - 1;
+			}
+		}
+		
+		System.out.println(answer);
+	}
+
+	private static int checkInstall(int dist) {
+		
+		int count = 1;
+		int lastInstalledHouse = house[0];
+		
+		for (int i = 1; i < house.length; i++) {
+			if (house[i] - lastInstalledHouse >= dist) {
+				count++;
+				lastInstalledHouse = house[i];
+			}
+//			else {
+//				break;
+//			}
+		}
+		
+		return count;
+	}
+
+}
