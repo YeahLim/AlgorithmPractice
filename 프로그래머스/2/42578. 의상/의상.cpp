0a7c0ded172a0a2b6dff4711f2d1a14f7782c667
Yeah'Lim <80014833+YeahLim@users.noreply.github.com>
2025-04-05 18:12:07 +0900

    [level 2] Title: 의상, Time: 0.03 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.cpp
+++ 프로그래머스/2/42578. 의상/의상.cpp
@@ -1,26 +1,19 @@
 #include <string>
 #include <vector>
 #include <unordered_map>
-#include <iostream>
 using namespace std;
 
 int solution(vector<vector<string>> clothes) {
     
     unordered_map<string, int> map;
     for (auto cloth : clothes) {
-        if (map.find(cloth[1]) == map.end()) {
-            map.insert(make_pair(cloth[1], 1));
-        }
-        else {
-            map[cloth[1]]++;
-        }
+        map[cloth[1]]++;
     }
     
     int answer = 1;
     
     for (const auto& pair : map) {
         answer *= (pair.second+1);
-        cout << pair.second << endl;
     }
     
     return answer - 1;
