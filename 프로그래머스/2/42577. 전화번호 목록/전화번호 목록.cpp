b9919d77d94946126ea31777cae4cbe70ff4e8b1
Yeah'Lim <80014833+YeahLim@users.noreply.github.com>
2025-04-05 17:35:20 +0900

    [level 2] Title: 전화번호 목록, Time: 81.63 ms, Memory: 35.6 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
@@ -7,19 +7,12 @@ using namespace std;
 bool solution(vector<string> phone_book) {
     bool answer = true;
     
-    sort(phone_book.begin(), phone_book.end(), [](const string& a, const string& b){
-        return a.length() < b.length();
-    });
+    sort(phone_book.begin(), phone_book.end());
     
-    unordered_set<string> set;
-    
-    for (auto phone : phone_book) {
-        for (int i = 0; i < phone.length(); i++) {
-            if (set.find(phone.substr(0, i)) != set.end()) {
-                return false;
-            }
+    for (int i = 1; i < phone_book.size(); i++) {
+        if (phone_book[i-1] == phone_book[i].substr(0, phone_book[i-1].size())) {
+            return false;
         }
-        set.insert(phone);
     }
     
     return true;
