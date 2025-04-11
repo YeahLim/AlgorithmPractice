cef2ce4f5a1a2f028a3c26b4ffc6ad3ed360c688
Yeah'Lim <80014833+YeahLim@users.noreply.github.com>
2025-04-11 23:32:11 +0900

    [level 2] Title: 기능개발, Time: 0.02 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.cpp
+++ 프로그래머스/2/42586. 기능개발/기능개발.cpp
@@ -1,33 +1,37 @@
 #include <string>
 #include <vector>
-#include <iostream>
 #include <cmath>
+#include <queue>
 
 using namespace std;
 
 vector<int> solution(vector<int> progresses, vector<int> speeds) {
     
     // 1. 기능 완성까지 걸리는 일수
-    vector<int> days;
+    queue<int> days;
     for (int i = 0; i < progresses.size(); i++) {
-        days.push_back(ceil((double)(100 - progresses[i]) / speeds[i]));
+        days.push(ceil((double)(100 - progresses[i]) / speeds[i]));
     }
     
     // 2. 배포
     vector<int> answer;
-    int tmp = days[0];
+    int tmp = days.front();
+    days.pop();
     int count = 1;
-    for (int i = 1; i < days.size(); i++) {
-        if (tmp >= days[i]) {
+    while (!days.empty()) {
+        if (days.front() <= tmp) {
+            days.pop();
             count++;
         }
         else {
             answer.push_back(count);
-            tmp = days[i];
             count = 1;
+            tmp = days.front();
+            days.pop();
         }
     }
     answer.push_back(count);
+    
     
     return answer;
 }
