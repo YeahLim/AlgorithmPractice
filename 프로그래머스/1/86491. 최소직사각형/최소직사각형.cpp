4fb2f6c0a16064e2d57c625eb6af1a0d01658d96
Yeah'Lim <80014833+YeahLim@users.noreply.github.com>
2025-04-12 14:32:40 +0900

    [level 1] Title: 최소직사각형, Time: 0.61 ms, Memory: 5.03 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.cpp
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.cpp
@@ -9,22 +9,9 @@ int solution(vector<vector<int>> sizes) {
     int max_col = 0;
     
     for (vector<int>& size : sizes) {
-        if (size[0] > size[1] ) {
-            if (max_row < size[0]) {
-                max_row = size[0];
-            }
-            if (max_col < size[1]) {
-                max_col = size[1];
-            }
-        }
-        else {
-            if (max_row < size[1]) {
-                max_row = size[1];
-            }
-            if (max_col < size[0]) {
-                max_col = size[0];
-            }
-        }
+        
+        max_row = max(max_row, max(size[0], size[1]));
+        max_col = max(max_col, min(size[0], size[1]));
     }
     
     return max_row * max_col;
