b316856771c5f598b3296356e906d6caa6c064fc
Yeah'Lim <80014833+YeahLim@users.noreply.github.com>
2025-04-13 00:37:04 +0900

    [level 1] Title: 모의고사, Time: 0.10 ms, Memory: 4.45 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.cpp
+++ 프로그래머스/1/42840. 모의고사/모의고사.cpp
@@ -9,28 +9,21 @@ vector<int> solution(vector<int> answers) {
     vector<int> second = {2,1,2,3,2,4,2,5};
     vector<int> third = {3,3,1,1,2,2,4,4,5,5};
     
-    int firstIndex = 0;
-    int secondIndex = 0;
-    int thirdIndex = 0;
-    
     vector<int> score(3,0);
     
     for (int i = 0; i < answers.size(); i++) {
         
-        if (answers[i] == first[firstIndex]) {
+        if (answers[i] == first[i % first.size()]) {
             score[0] += 1;
         }
-        firstIndex = (firstIndex + 1) % first.size();
         
-        if (answers[i] == second[secondIndex]) {
+        if (answers[i] == second[i % second.size()]) {
             score[1] += 1;
         }
-        secondIndex = (secondIndex + 1) % second.size();
         
-        if (answers[i] == third[thirdIndex]) {
+        if (answers[i] == third[i % third.size()]) {
             score[2] += 1;
         }
-        thirdIndex = (thirdIndex + 1) % third.size();
     }
     
     int maxScore = *max_element(score.begin(), score.end());
