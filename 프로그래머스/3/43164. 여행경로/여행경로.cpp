07424eecdd1ccf10f935542d9d2b6ae287201d8d
Yeah'Lim <80014833+YeahLim@users.noreply.github.com>
2025-04-20 20:30:10 +0900

    [level 3] Title: 여행경로, Time: 0.04 ms, Memory: 3.73 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.cpp
+++ 프로그래머스/3/43164. 여행경로/여행경로.cpp
@@ -1,31 +1,29 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <iostream>
 using namespace std;
 
 vector<vector<string>> tickets;
 vector<bool> visited;
+bool isSearched;
 vector<string> answer;
 
 void searchRoute(string airport, int depth, vector<string> route) {
-    
-    if (depth == tickets.size()) {
-        
-        // 첫번째 경로
-        if (answer.empty()) {
-            answer = route;
-        }
-        
-        // 더 작은 경로가 나온 경우
-        if (route < answer) {
-            answer = route;
-        }
+   
+    if (isSearched) {
         return;
     }
-    
+   
+    if (depth == tickets.size()) {
+        isSearched = true;
+        answer = route;
+        return;
+    }
+   
     for (int i = 0; i < tickets.size(); i++) {
         
-        if (!visited[i] && airport == tickets[i][0]) {
+        if (!visited[i] && airport == tickets[i][0]) {   
             visited[i] = true;
             route.push_back(tickets[i][1]);
             searchRoute(tickets[i][1], depth+1, route);
@@ -37,11 +35,15 @@ void searchRoute(string airport, int depth, vector<string> route) {
 
 vector<string> solution(vector<vector<string>> tickets) {
 
-    ::tickets = tickets;
     visited = vector<bool>(tickets.size(), false);
-    
-    // dfs로 루트 구하기
+    isSearched = false;
+   
+    // 1. tickets 알파벳 정렬
+    sort(tickets.begin(), tickets.end());
+    ::tickets = tickets;
+
+    // 2. dfs로 루트 구하기
     searchRoute("ICN", 0, {"ICN"});
-    
+   
     return answer;
 }
