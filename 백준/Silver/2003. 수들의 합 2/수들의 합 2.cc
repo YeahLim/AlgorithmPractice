dc8467deded8d58b7089f5f361763b1720266e7d
Yeah'Lim <80014833+YeahLim@users.noreply.github.com>
2025-04-25 20:50:38 +0900

    [Silver IV] Title: 수들의 합 2, Time: 0 ms, Memory: 2180 KB -BaekjoonHub

---
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.cc
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main() {
+
+    // 입력
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n, m;
+    cin >> n >> m;
+
+    vector<int> array(n);
+    for (int i = 0; i < n; i++) {
+        cin >> array[i];
+    }
+    
+    // 투 포인터로 합이 m이 되는 경우 구하기
+    int left = 0;
+    int right = 0;
+    int sum = array[left];
+    int answer = sum == m ? 1: 0;
+    while (right < n) {
+
+        if (sum <= m) {
+            right++;
+            if (right == n) break;
+            sum += array[right];
+        }
+        else if (sum > m) {
+            sum -= array[left];
+            left++;
+        }
+
+        if (sum == m) {
+            answer++;
+        }
+
+    }
+
+
+    // 출력
+    cout << answer;
+}
