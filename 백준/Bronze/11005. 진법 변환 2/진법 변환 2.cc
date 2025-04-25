b39755eb01718b2bfcce2b3464cafdc2e4beef62
Yeah'Lim <80014833+YeahLim@users.noreply.github.com>
2025-04-26 00:03:16 +0900

    [Bronze I] Title: 진법 변환 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/11005. 진법 변환 2/진법 변환 2.cc
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.cc
@@ -8,24 +8,11 @@ int main() {
     int num, base;
     cin >> num >> base;
 
-    // 10부터 알파벳 변환
-    unordered_map<int, char> alphabets;
-    for (int i = 10; i < 36; i++) {
-        alphabets[i] = 65 + i - 10;
-    }
+    string digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
     string tx_num = "";
-    while (num >= 1) {
-        
-        int rest = num % base; // 나머지
-        
-        if (rest >= 10) {
-            tx_num = alphabets[rest] + tx_num;
-        }
-        else {
-            tx_num = to_string(rest) + tx_num;
-        }
-
+    while (num > 0) {    
+        tx_num = digits[num % base] + tx_num;
         num /= base; // 나누기
     }
 
