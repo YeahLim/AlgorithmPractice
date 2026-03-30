2205676f776b748ef6ad67a6bce09484915c51a0
Yeah'Lim <80014833+YeahLim@users.noreply.github.com>
2026-03-30 23:11:14 +0900

    [level 3] Title: 네트워크, Time: 0.07 ms, Memory: 3.81 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/README.md
+++ 프로그래머스/3/43162. 네트워크/README.md
@@ -1,10 +1,10 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=cpp) 
 
 ### 성능 요약
 
-메모리: 79.3 MB, 시간: 0.49 ms
+메모리: 3.81 MB, 시간: 0.07 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 03월 30일 23:01:12
+2026년 03월 30일 23:11:14
 
 ### 문제 설명
 
--- 프로그래머스/3/43162. 네트워크/네트워크.cpp
+++ 프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -1,26 +1,19 @@
-#include <string>
-#include <vector>
-#include <queue>
+#include <bits/stdc++.h>
+
 using namespace std;
 
-int n;
 vector<vector<int>> computers;
-vector<bool> visited; 
 
-void searchNetworks(int curr, int count) {
-    
-    queue<int> q; // 컴퓨터 큐
-    q.push(curr);
-    visited[curr] = true;
+vector<bool> searchConnectedComputers(vector<bool> visited, int start) {
+    queue<int> q;
+    q.push(start);
+    visited[start] = true;
     
     while (!q.empty()) {
-        
         int curr = q.front();
         q.pop();
         
         for (int i = 0; i < computers[curr].size(); i++) {
-            
-            // 아직 방문하지 않고 연결돼있는 경우
             if (!visited[i] && computers[curr][i] == 1) {
                 visited[i] = true;
                 q.push(i);
@@ -28,26 +21,22 @@ void searchNetworks(int curr, int count) {
         }
     }
     
-    
+    return visited;
 }
 
 int solution(int n, vector<vector<int>> computers) {
     
-    ::n = n;
     ::computers = computers;
-    ::visited = vector<bool>(n, false);
+    vector<bool> visited(n, false);
+    int answer = 0;
     
-    // bfs로 네트워크 탐색
-    int count = 0;
     for (int i = 0; i < n; i++) {
         
-        // 네트워크 탐색을 아직 안 한 경우
-        if (visited[i] == 0) {
-            count++;
-            searchNetworks(i, count);
+        if (!visited[i]) {
+            visited = searchConnectedComputers(visited, i);
+            answer++;
         }
-        
     }
     
-    return count;
+    return answer;
 }
