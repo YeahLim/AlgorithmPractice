e57dd338fc0e4f183550e35840114352a4f3cbda
Yeah'Lim <80014833+YeahLim@users.noreply.github.com>
2026-04-19 20:22:22 +0900

    [Gold V] Title: A와 B 2, Time: 104 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Gold/12919. A와 B 2/A와 B 2.java
+++ 백준/Gold/12919. A와 B 2/A와 B 2.java
@@ -8,59 +8,53 @@ import java.util.Set;
 
 public class Main {
 	
+	
 	public static void main(String[] args) throws IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String S = br.readLine();
 		String T = br.readLine();
 		
-		int answer = 0;
-		
-		Queue<String> q = new LinkedList<>();
-		q.add(T);
-		
-		Set<String> set = new HashSet<>();
-		
-		while (!q.isEmpty()) {
-			String curr = q.poll();
-			
-			// 처음값과 같은 경우
-			if (curr.equals(S)) {
-				answer = 1;
-				break;
-			}
-			
-			if (curr.length() == S.length()) {
-				continue;
-			}
-
-			String newCurr;
-			
-			// 문자열의 마지막이 A인 경우 제거
-			if (curr.charAt(curr.length() - 1) == 'A') {
-				newCurr = curr.substring(0, curr.length() - 1); 
-				
-				if (!set.contains(newCurr)) {
-					q.add(newCurr);
-					set.add(newCurr);
-				}
-			}
-			
-			// 문자열의 첫번째가 B인 경우 뒤집고 마지막 B 제거
-			if (curr.charAt(0) == 'B') {
-				newCurr = new StringBuilder(curr).reverse().substring(0, curr.length() - 1).toString(); 
-				
-				if (!set.contains(newCurr)) {
-					q.add(newCurr);
-					set.add(newCurr);
-				}
-			} 
-			
-//			System.out.println(curr);
-			
+		if (convertTtoS(T, S, new HashSet<>())) {
+			System.out.println(1);
+		} else {
+			System.out.println(0);
 		}
 		
-		System.out.println(answer);
+	}
+
+	private static boolean convertTtoS(String curr, String target, Set<String> set) {
+		
+		// 목표값과 같은 경우
+		if (curr.equals(target)) {
+			return true;
+		}
+		
+		if (curr.length() == target.length()) {
+			return false;
+		}
+		
+		// 마지막이 A로 끝나는 경우 제거
+		if (curr.charAt(curr.length() - 1) == 'A') {
+			String next = curr.substring(0, curr.length() - 1);
+			
+			if (!set.contains(next)) {
+				set.add(next);
+				if (convertTtoS(next, target, set)) return true;
+			}
+		}
+		
+		// 첫번째가 B로 끝나는 경우, 뒤집고 마지막 제거
+		if (curr.charAt(0) == 'B') {
+			String next = new StringBuilder(curr).reverse().substring(0, curr.length() - 1);
+			
+			if (!set.contains(next)) {
+				set.add(next);
+				if (convertTtoS(next, target, set)) return true;
+			}
+		}
+		
+		return false;
 	}
 
 }
